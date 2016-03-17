7df0498ff2fc0d36698d588b87a6106cf04fdeb7
Katie Reid <kar888@hotmail.co.uk>
2016-03-17 11:44:25 +0000

    ace4_v6.c
    Get path now working but when we have more than one unrecognised input the unknown command prints too many times this still needs to be fixed.

---
+++ ace4_v6.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+#define MAX 512
+#define pointer ">"
+
+char *par[MAX] = {NULL};
+char input[1000];
+const char* home;
+
+void exit(int status); /*function for exiting the program*/
+
+void executeCMD(){
+
+	int status;
+	pid_t pid;
+	/* fork a child process */
+	pid = fork();
+	
+	if (pid == 0) { /* child process */
+		if (execvp(par[0], par)){
+			printf("%s is an Unknown Command\n", *par);
+		exit(0);
+		}
+	}
+	else if (pid < 0) { /* error occurred */
+		printf("Fork Failed");
+		exit(1);
+	}
+	else { /* parent process */
+		/* parent will wait for the child to complete */
+		while(wait(&status)!=pid);
+	}	
+}
+
+
+
+
+void runInternal(){
+	int ex;
+	int getp;
+	int i;
+	for(i = 0; i < MAX - 1; i++) {
+		if(par[i] != NULL) {
+			ex = strcmp(par[i],"exit");
+			getp = strcmp(par[0], "getpath");
+
+			if(getp == 0) {
+				if(par[1] != NULL){
+					printf("%s is an Unknown command\n", *par);
+				}
+				else{
+					const char* path = getenv("PATH");
+					printf("PATH :%s\n",(path!=NULL)? path : "getenv returned NULL");
+				}
+			}
+			else if(ex == 0) {
+				exit(0);
+			}
+			else{
+				executeCMD();
+			}
+		}	
+	}
+}
+
+void tokenise(){
+	char tokeniser[50] = "\t \n <>&|;";
+	char *p = strtok(input, tokeniser);
+	int i = 0;
+			
+	if(NULL == p){
+		return;
+	}
+	
+	while(p != NULL){
+		par[i] = p;
+		++i;
+		printf("(%s)\n", p);
+		p = strtok(NULL, tokeniser);
+	}
+	runInternal();
+}
+
+void getInput(){
+	/*char input[1000];*/
+
+	while(1){
+		printf(pointer);
+		if(fgets(input, MAX, stdin) == NULL){
+			exit(0);
+		}
+		tokenise();
+	}
+}
+
+
+
+int main(){
+const char* path = getenv("PATH");
+	home = getenv("HOME");
+	chdir(home);
+	printf("PATH :%s\n",(path!=NULL)? path : "getenv returned NULL");	
+	printf("Home: %s\n ",home); 	
+	getInput();
+	return(0);
+}
