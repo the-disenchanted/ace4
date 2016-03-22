ed4b11ea541ae7f20a5b39b12bd2ddfd2790833d
Katie Reid <kar888@hotmail.co.uk>
2016-03-22 16:36:41 +0000

    get path and set path now work but cannot run shell from within itself!

---
+++ ace4_v7.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+#define MAX 512
+#define pointer ">"
+
+char *par[MAX];
+char input[1000];
+const char* home;
+
+
+char *inter[] = {"exit", "getpath", "setpath"};
+void exit(int status); /*function for exiting the program*/
+
+void executeCMD(){
+
+	int status;
+	pid_t pid;
+	/* fork a child process */
+	pid = fork();
+	
+	if (pid == 0) { /* child process */
+		if (execvp(par[0], par)){
+			printf("%s is an Unknown Command 1\n", *par);
+		exit(0);
+		}
+	}
+	else if (pid < 0) { /* error occurred */
+		printf("Fork Failed");
+		exit(1);
+	}
+	else { /* parent process */
+		/* parent will wait for the child to complete */
+		while(wait(&status)!=pid);
+	}	
+}
+
+
+void runInternal(int m){
+
+	switch(m){
+		case 0:
+			//int restore = setenv(home, home, 0);
+			home = getenv("HOME");
+			printf("HOME: %s\n", home);
+			exit(0);
+		case 1:
+			/*if(par[1] != NULL){
+					printf("%s is an Unknown command 2\n", *par);
+			}
+			else{*/
+				printf("PATH = %s\n", getenv("PATH"));
+				/*}*/
+			break;
+		case 2:
+			if(par[1] == NULL){
+					printf("%s is an Unknown command 3\n", *par);
+			}
+			else{
+				int newPath = setenv(par[1], par[1], 0);
+				printf("%d", newPath);
+				/*const char* path = getenv("PATH");
+				printf("PATH :%s\n",(path!=NULL)? path : "getenv returned NULL");*/
+			}
+		}	
+}
+
+void run(){
+	int m;
+	int intersize = sizeof(inter)/sizeof(char *);
+	int in;
+	in = 0;
+	
+	if(par[0] == NULL){
+		printf("No command entered");
+	}
+	
+	for(m = 0; m < intersize; m++){
+		if(strcmp(par[0], inter[m]) == 0){
+			in = 1;
+			runInternal(m);
+		}
+	}
+	if(in == 0) executeCMD();
+}
+
+void tokenise(){
+	char tokeniser[50] = "\t \n <>&|;";
+	char *p = strtok(input, tokeniser);
+	int i = 0;
+			
+	if(NULL == p){
+		return;
+	}
+	
+	while(p != NULL){
+		par[i] = p;
+		++i;
+		printf("(%s)\n", p);
+		p = strtok(NULL, tokeniser);
+	}
+	run();
+}
+
+void getInput(){
+	/*char input[1000];*/
+
+	while(1){
+		printf(pointer);
+		if(fgets(input, MAX, stdin) == NULL){
+			exit(0);
+		}
+		tokenise();
+	}
+}
+
+int main(){
+const char* path = getenv("PATH");
+	home = getenv("HOME");
+	chdir(home);
+	printf("PATH :%s\n",(path!=NULL)? path : "getenv returned NULL");	
+	printf("Home: %s\n ",home); 	
+	getInput();
+	return(0);
+}
